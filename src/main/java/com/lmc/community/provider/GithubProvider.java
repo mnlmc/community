29eb8b2dea4cc6a74bd4a59ae4b159c4a62ff452
e <limaochong@sbr-info.com>
2019-07-17 11:16:29 +0800

    a

---
+++ src/main/java/com/lmc/community/provider/GithubProvider.java
@@ -0,0 +1,26 @@
+package com.lmc.community.provider;
+
+import com.lmc.community.dto.AccessTokenDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubProvider {
+
+    public String getAccesToken(AccessTokenDTO accessTokenDTO){
+        public static final MediaType JSON
+                = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        String post(String url, String json) throws IOException {
+            RequestBody body = RequestBody.create(JSON, json);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            }
+        }
+    }
+}
