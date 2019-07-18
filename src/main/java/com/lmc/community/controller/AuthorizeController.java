b156900ff8584b635c0d1dcb733b544afe1df1d9
e <limaochong@sbr-info.com>
2019-07-18 15:31:38 +0800

    extract github config  from code to properties

--- src/main/java/com/lmc/community/controller/AuthorizeController.java
+++ src/main/java/com/lmc/community/controller/AuthorizeController.java
@@ -1,14 +1,41 @@
 package com.lmc.community.controller;
 
+import com.lmc.community.dto.AccessTokenDTO;
+import com.lmc.community.dto.GithubUser;
+import com.lmc.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
-@GetMapping("/callback")
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
     public String callBack(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
 
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUrl);
+        accessTokenDTO.setState(state);
+        String accesToken = githubProvider.getAccesToken(accessTokenDTO);
+        System.out.println("accesdtoken=="+accesToken);
+        GithubUser user = githubProvider.getUser(accesToken);//user信息
 
 
         return "index";
--- src/main/java/com/lmc/community/provider/GithubProvider.java
+++ src/main/java/com/lmc/community/provider/GithubProvider.java
@@ -1,26 +1,51 @@
 package com.lmc.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.lmc.community.dto.AccessTokenDTO;
+import com.lmc.community.dto.GithubUser;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.io.IOException;
+
+@Component//把普通pojo实例化到spring容器中
 public class GithubProvider {
 
     public String getAccesToken(AccessTokenDTO accessTokenDTO){
-        public static final MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
-
+        MediaType mediaType= MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
-        String post(String url, String json) throws IOException {
-            RequestBody body = RequestBody.create(JSON, json);
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
             Request request = new Request.Builder()
-                    .url(url)
+                    .url("https://github.com/login/oauth/access_token")
                     .post(body)
                     .build();
+        System.out.println("cccccccccccccc");
             try (Response response = client.newCall(request).execute()) {
-                return response.body().string();
+                String string = response.body().string();
+                String token = string.split("&")[0].split("=")[1];
+                System.out.println(token);
+                return token;
+            } catch (IOException e) {
             }
+            return  null;
+
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://api.github.com/user?access_token="+accessToken)
+                    .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            System.out.println("String========="+string);
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
         }
+        return null;
+
     }
 }
